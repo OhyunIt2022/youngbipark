a1529d6645aef5c71266b2c44281474613a0fe37
younbi12 <a94290220@gmail.com>
2022-08-02 18:31:43 +0900

    cm

---
+++ bookmarks/views.py
@@ -0,0 +1,15 @@
+from multiprocessing import context
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+from bookmarks.models import Bookmark
+
+
+def index(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+
+def bookmark_list(request):
+    bookmark_list = Bookmark.objects.all()
+    context = {'bookmark_list':bookmark_list}
+    return render(request, 'templates/bookmark_list.html', context)
